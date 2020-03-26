4113193728f3dabed097fb45ad09bfe6cee5dbfa
Logan Negley <logannegley@gmail.com>
2020-03-26 18:28:14 -0500

    Affiliation comp added to App

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,12 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import './App.css';
+// import './App.css';
 
 import Navigation from '../src/components/Navigation';
+import Affiliations from '../src/components/Affiliations';
 
 function App() {
   return (
     <div className="App">
       <Navigation/>
+      <Affiliations/>
     </div>
   );
 }
