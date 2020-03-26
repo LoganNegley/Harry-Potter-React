f3ea7c85ea95aa29988cef18ab4335f7b9bd88f7
Logan Negley <logannegley@gmail.com>
2020-03-26 17:45:19 -0500

    Navigation component created

---
+++ my-app/src/components/Navigation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="nav-container">
+      <nav>
+        <button>Home</button>
+        <button>Sorting Hat</button>
+        <button>Hogwarts Express</button>
+      </nav>
+    </div>
+  );
+}
+
+export default App;
