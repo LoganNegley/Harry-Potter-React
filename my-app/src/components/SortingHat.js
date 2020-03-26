1f2c655a4c6dce3cc1509a52825eacf9bbbbec5a
Logan Negley <logannegley@gmail.com>
2020-03-26 18:04:24 -0500

    SortingHat, MainCarousel components created

---
+++ my-app/src/components/SortingHat.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+function SortingHat() {
+  return (
+    <div className='sorting-hat-form'>
+        <form>
+            
+        </form>
+    </div>
+  );
+}
+
+export default SortingHat;
