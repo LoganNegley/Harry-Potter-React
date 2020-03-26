5bc762eb7927315013a6da5d03375bfecc667b48
Logan Negley <logannegley@gmail.com>
2020-03-26 18:26:41 -0500

    Affiliation component elements added

---
+++ my-app/src/components/Affiliations.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+
+function Affiliations() {
+  return (
+    <div className="affiliations-container">
+        <section>
+            <h3>Dumbledore's Army</h3>
+            <p>image goes here</p>
+            <button>Whos in Dubledore's Army</button>
+        </section>
+        <section>
+            <h3>Order Of The Phoenix</h3>
+            <p>image goes here</p>
+            <button>Whos in the Order Of The Phoenix</button>
+        </section>
+        <section>
+            <h3>Ministry Of Magic</h3>
+            <p>image goes here</p>
+            <button>Whos part of the Ministry Of Magic</button>
+        </section>
+
+    </div>
+  );
+}
+
+export default Affiliations;
