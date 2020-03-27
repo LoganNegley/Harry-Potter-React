643b7e1124c5719c16710c097ca8512ae0ae0c89
Logan Negley <logannegley@gmail.com>
2020-03-26 22:10:37 -0500

    got images in carousel to resize correctly

--- my-app/src/components/MainCarousel.js
+++ my-app/src/components/MainCarousel.js
@@ -10,38 +10,30 @@ function MainCarousel() {
         <Carousel.Item>
             <img
             className="d-block w-100"
-            src={require('../images/papers')}
+            src='images/papers.jpg'
             alt="First slide"
             />
             <Carousel.Caption>
-            <h3>First slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Second slide&bg=282c34"
+            src="images/spell.jpg"
             alt="Second slide"
             />
 
             <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
             </Carousel.Caption>
         </Carousel.Item>
         <Carousel.Item>
             <img
             className="d-block w-100"
-            src="holder.js/800x400?text=Third slide&bg=20232a"
+            src="images/ron.jfif"
             alt="Third slide"
             />
 
             <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
             </Carousel.Caption>
         </Carousel.Item>
         </Carousel>
