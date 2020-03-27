5706f213fa9209624766d220cd19d392a8ff5135
Logan Negley <logannegley@gmail.com>
2020-03-26 22:53:25 -0500

    affiliation component changed in JXS

--- my-app/src/components/Affiliations.js
+++ my-app/src/components/Affiliations.js
@@ -8,19 +8,31 @@ function Affiliations() {
     <MainCarousel/>
     <div className="affiliations-container">
         <section>
-            <h3>Dumbledore's Army</h3>
+          <div className='dbArmy-img'>
             <p>image goes here</p>
+          </div>
+          <div className='dbArmy-quote'>
+            <p>quote</p>
             <button>Whos in Dubledore's Army</button>
+          </div>
         </section>
         <section>
-            <h3>Order Of The Phoenix</h3>
+          <div className='phoenix-img'>
             <p>image goes here</p>
+          </div>
+          <div className='phoenix-quote'>
+            <p>quote</p>
             <button>Whos in the Order Of The Phoenix</button>
+          </div>
         </section>
         <section>
-            <h3>Ministry Of Magic</h3>
+            <div className='ministry-img'>
             <p>image goes here</p>
+          </div>
+          <div>
+            <p>quote</p>
             <button>Whos part of the Ministry Of Magic</button>
+          </div>
         </section>
     </div>
     </>
