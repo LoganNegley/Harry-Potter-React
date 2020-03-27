5f70926b7734ca323ad7c4760c4ba9c6641ad727
Logan Negley <logannegley@gmail.com>
2020-03-26 19:02:37 -0500

    app.css styles added and inline black background on body

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react';
-// import './App.css';
+import './App.css';
 
 import Navigation from '../src/components/Navigation';
 import Affiliations from '../src/components/Affiliations';
