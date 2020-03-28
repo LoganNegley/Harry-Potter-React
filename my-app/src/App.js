0a7a58b72c460add805b47a4ac9dcdf2bb4237d5
Logan Negley <logannegley@gmail.com>
2020-03-27 20:05:17 -0500

    react router added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,21 @@
 import React, {useEffect, useState} from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
 
 import Navigation from '../src/components/Navigation';
 import Affiliations from '../src/components/Affiliations';
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+
   return (
-    <div className="App">
+<>
       <Navigation/>
       <Affiliations/>
-    </div>
+
+
+</>
   );
 }
 
