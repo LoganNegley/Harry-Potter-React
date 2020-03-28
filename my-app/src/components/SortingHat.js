c482c51befb1f3070e05e88168898501de26caa3
Logan Negley <logannegley@gmail.com>
2020-03-28 14:27:36 -0500

    sorting hat image added to component

--- my-app/src/components/SortingHat.js
+++ my-app/src/components/SortingHat.js
@@ -2,11 +2,13 @@ import React from 'react';
 
 
 function SortingHat() {
+  const houses = ['Hufflepuff,Slytherin,Ravenclaw,Gryffindor']
+
   return (
-    <div className='sorting-hat-form'>
-        <form>
-            
-        </form>
+    <div className='sorting-hat'>
+    <img src='images/sorting.jpg'/>
+    <p>Here you can take a quiz and determine witch Hogwarts house you would belong to. </p>
+    <button>Take The Quiz</button>
     </div>
   );
 }
