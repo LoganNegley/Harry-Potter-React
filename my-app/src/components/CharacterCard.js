4ca136b575e4b70c5c63aec9fde073c93e34a7b9
Logan Negley <logannegley@gmail.com>
2020-03-28 19:17:03 -0500

    no changes

--- my-app/src/components/CharacterCard.js
+++ my-app/src/components/CharacterCard.js
@@ -1,19 +1,16 @@
 import React,{useContext} from 'react';
-import {Spinner} from 'reactstrap';
 import {CharacterContext} from '../context/CharacterContext';
 
 
-function CharacterCard() {
 
-const characters = useContext(CharacterContext);
-console.log(characters)
+function CharacterCard(props) {
+  const characters = useContext(CharacterContext);
 
   return (
     <div className="character-card-container">
-      {!characters ?   <Spinner style={{ width: '3rem', height: '3rem', color:'yellow' }} type="grow" /> :
-      characters.map(character =>(
+ {props.characters.map(character =>(
         <div className='character-card'>
-         <h1> {characters.name}</h1>
+         <h1> {character.name}</h1>
          <p> {character.role}</p>
          <p> {character.school}</p>
         </div>
--- my-app/src/components/DumbledoreArmy.js
+++ my-app/src/components/DumbledoreArmy.js
@@ -1,11 +1,19 @@
-import React from 'react';
+import React,{useContext} from 'react';
+import {Spinner} from 'reactstrap';
+import {CharacterContext} from '../context/CharacterContext';
 import CharacterCard from '../components/CharacterCard';
 
 
-function DumbledoreArmy(props) {
+function DumbledoreArmy() {
+  const characters = useContext(CharacterContext);
+console.log(characters)
+
+
   return (
     <div className="dumbledore-army-container">
-        <CharacterCard characters={props.characters}/>
+      {!characters 
+      ?  <Spinner style={{ width: '3rem', height: '3rem', color:'yellow' }} type="grow" /> 
+      :  }
     </div>
   );
 }
