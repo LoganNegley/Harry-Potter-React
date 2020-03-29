295e893629a9105a50d273cc185354a2c0c5a95b
Logan Negley <logannegley@gmail.com>
2020-03-28 21:51:02 -0500

    api for spells with useEffect

--- my-app/src/components/Spellbook.js
+++ my-app/src/components/Spellbook.js
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
+import axios from 'axios';
 
 
 function Navigation() {
+const [spells, setSpells] = useState([]);
+
+
+useEffect(()=>{
+    axios.get('https://www.potterapi.com/v1/spells?key=$2a$10$Q4u.rpQuXNlJGGU3sga/g.iojAEbxt6kaul2QD6wr.ZqH9u6oh2fS')
+    .then(response => {
+        setSpells(response.data)
+
+    })
+    .catch(error => {
+        console.log(error, 'error with getting spells from api')
+    })
+},[])
+        console.log(spells)
+
+
+
   return (
     <div className="spellbook-container">
-        <h1>The Spellbook</h1>
+
     </div>
   );
 }
