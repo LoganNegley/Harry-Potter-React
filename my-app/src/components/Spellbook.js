034d45b7b6b04257b2967715985ad06e4e8c32e4
Logan Negley <logannegley@gmail.com>
2020-03-31 07:23:29 -0500

    no changes

--- my-app/src/components/Spellbook.js
+++ my-app/src/components/Spellbook.js
@@ -7,7 +7,8 @@ import FlipPage from 'react-flip-page'
 
 function Spellbook() {
 const [spells, setSpells] = useState([]);
-
+const array = ['item one', 'item two', 'item three']
+const [pageCount, setPageCount]= useState(0);
 
 // useEffect(()=>{
 //     axios.get('https://www.potterapi.com/v1/spells?key=$2a$10$Q4u.rpQuXNlJGGU3sga/g.iojAEbxt6kaul2QD6wr.ZqH9u6oh2fS')
@@ -19,27 +20,43 @@ const [spells, setSpells] = useState([]);
 //     })
 // },[])
 
-  return (
+const pageNumber= () => {
+setPageCount(pageCount + 1)
+};
 
-      <FlipPage 
-      orientation='horizontal'
-      pageBackground='black'
-      >
-  <article>
-    <h1>My awesome first article</h1>
-    <p>My awesome first content</p>
-    <p>Hello again</p>
-  </article>
-  <article>
-    <h1>My wonderful second article</h1>
-    <p>My wonderful second content</p>
-  </article>
-  <article>
-    <h1>My excellent third article</h1>
-    <p>My excellent third content</p>
-  </article>
-</FlipPage>
+  return (
+  <div className='spell-book-container'>
+    <FlipPage 
+    className='flipBook'
+    orientation='horizontal'
+    pageBackground='white'
+    uncutePages ='true'
+    width='100%'
+    >
+    {array.map(item =>(
+    <article>
+    <p>{pageNumber}</p>
+    <h1>{item}</h1>
+    </article>
+    ))}
+    </FlipPage>
+  </div>
   );
 }
 
 export default Spellbook;
+
+
+//  <article>
+//     <h1>My awesome first article</h1>
+//     <p>My awesome first content</p>
+//     <p>Hello again</p>
+//   </article>
+//   <article>
+//     <h1>My wonderful second article</h1>
+//     <p>My wonderful second content</p>
+//   </article>
+//   <article>
+//     <h1>My excellent third article</h1>
+//     <p>My excellent third content</p>
+//   </article>
