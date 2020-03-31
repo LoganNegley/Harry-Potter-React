1b6c22eac92dc5fa708a4f03a2ef7a526bce193a
Logan Negley <logannegley@gmail.com>
2020-03-31 07:55:52 -0500

    tried jquery didn't work

--- my-app/src/components/Spellbook.js
+++ my-app/src/components/Spellbook.js
@@ -3,12 +3,12 @@ import {Link} from 'react-router-dom';
 import axios from 'axios';
 import {Spinner} from 'reactstrap';
 import FlipPage from 'react-flip-page'
+import $ from 'jquery';
 
 
 function Spellbook() {
 const [spells, setSpells] = useState([]);
 const array = ['item one', 'item two', 'item three']
-const [pageCount, setPageCount]= useState(0);
 
 // useEffect(()=>{
 //     axios.get('https://www.potterapi.com/v1/spells?key=$2a$10$Q4u.rpQuXNlJGGU3sga/g.iojAEbxt6kaul2QD6wr.ZqH9u6oh2fS')
@@ -20,31 +20,37 @@ const [pageCount, setPageCount]= useState(0);
 //     })
 // },[])
 
-const pageNumber= () => {
-setPageCount(pageCount + 1)
-};
-
   return (
   <div className='spell-book-container'>
-    <FlipPage 
-    className='flipBook'
-    orientation='horizontal'
-    pageBackground='white'
-    uncutePages ='true'
-    width='100%'
-    >
-    {array.map(item =>(
-    <article>
-    <p>{pageNumber}</p>
-    <h1>{item}</h1>
-    </article>
-    ))}
-    </FlipPage>
+    <div id='spellBook'>
+      <div>Page One</div>
+      <div>Page Two</div>
+      <div>Page Three</div>
+      <div>Page Four</div>
+      <div>Page Five</div>
+      <div>Page Six</div>
+    </div>
   </div>
   );
 }
 
 export default Spellbook;
+
+
+    // <FlipPage 
+    // className='flipBook'
+    // orientation='horizontal'
+    // pageBackground='white'
+    // uncutePages ='true'
+    // width='100%'
+    // >
+    // {array.map(item =>(
+    // <article>
+    // <p>{pageNumber}</p>
+    // <h1>{item}</h1>
+    // </article>
+    // ))}
+    // </FlipPage>
 
 
 //  <article>
