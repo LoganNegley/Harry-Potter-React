056ff1a7419dd95206dc1171ffc084c2d4ff6f78
Logan Negley <logannegley@gmail.com>
2020-03-31 06:47:22 -0500

    flipbook react component spellboook done

--- my-app/src/components/Spellbook.js
+++ my-app/src/components/Spellbook.js
@@ -2,26 +2,43 @@ import React, {useEffect, useState} from 'react';
 import {Link} from 'react-router-dom';
 import axios from 'axios';
 import {Spinner} from 'reactstrap';
+import FlipPage from 'react-flip-page'
 
 
 function Spellbook() {
 const [spells, setSpells] = useState([]);
 
 
-useEffect(()=>{
-    axios.get('https://www.potterapi.com/v1/spells?key=$2a$10$Q4u.rpQuXNlJGGU3sga/g.iojAEbxt6kaul2QD6wr.ZqH9u6oh2fS')
-    .then(response => {
-        setSpells(response.data)
-    })
-    .catch(error => {
-        console.log(error, 'error with getting spells from api')
-    })
-},[])
+// useEffect(()=>{
+//     axios.get('https://www.potterapi.com/v1/spells?key=$2a$10$Q4u.rpQuXNlJGGU3sga/g.iojAEbxt6kaul2QD6wr.ZqH9u6oh2fS')
+//     .then(response => {
+//         setSpells(response.data)
+//     })
+//     .catch(error => {
+//         console.log(error, 'error with getting spells from api')
+//     })
+// },[])
 
   return (
-    <div className="book-bg">
-      <div className='book-cover'></div>
-    </div>
+
+      <FlipPage 
+      orientation='horizontal'
+      pageBackground='black'
+      >
+  <article>
+    <h1>My awesome first article</h1>
+    <p>My awesome first content</p>
+    <p>Hello again</p>
+  </article>
+  <article>
+    <h1>My wonderful second article</h1>
+    <p>My wonderful second content</p>
+  </article>
+  <article>
+    <h1>My excellent third article</h1>
+    <p>My excellent third content</p>
+  </article>
+</FlipPage>
   );
 }
 
