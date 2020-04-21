ae022ebd9aa944adb0e590a4ac3720784b6a11d9
Logan Negley <logannegley@gmail.com>
2020-04-21 14:43:37 -0500

    quiz results component made

---
+++ my-app/src/components/QuizResults.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from 'react';
+
+function QuizResults(){
+
+
+    return (
+        <div className='quizResults'>
+
+        </div>
+    );
+};
+
+export default QuizResults;
