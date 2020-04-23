014e6a314ede12375db94847cd9b548264f4098f
Logan Negley <logannegley@gmail.com>
2020-04-23 09:00:33 -0500

    quizresults added with count

--- my-app/src/components/QuizResults.js
+++ my-app/src/components/QuizResults.js
@@ -2,11 +2,23 @@ import React, {useState, useEffect} from 'react';
 
 function QuizResults({houses, count}){
 const [selectedHouse, setSelectedHouse] = useState('');
-   
+
+useEffect(() =>{
+if(count === 5 && count <= 9){
+    setSelectedHouse('Gryffindor')
+} else if(count ===10 && <= 14){
+    setSelectedHouse('Hufflepuff')
+}else if(count === 15 && <= 17){
+    setSelectedHouse('Ravenclaw')
+} else if(count >= 18 ){
+    setSelectedHouse('Slytherin')
+}
+},[]);
+
     return (
         <div className='quizResults'>
         <h2>Your Hogwarts house is....</h2>
-           
+           {selectedHouse}
         </div>
     );
 };
