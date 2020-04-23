2c12c24e36ffa36abdfb5690ea1b537e2d6480da
Logan Negley <logannegley@gmail.com>
2020-04-23 09:49:57 -0500

    fixed QuizResult bug with adding count and figuring house

--- my-app/src/components/QuizResults.js
+++ my-app/src/components/QuizResults.js
@@ -6,15 +6,15 @@ const [selectedHouse, setSelectedHouse] = useState('');
 useEffect(() =>{
 if(count === 5 && count <= 9){
     setSelectedHouse('Gryffindor')
-} else if(count ===10 && <= 14){
+} else if(count === 10 || count <= 14){
     setSelectedHouse('Hufflepuff')
-}else if(count === 15 && <= 17){
+}else if(count === 15 || count <= 17){
     setSelectedHouse('Ravenclaw')
 } else if(count >= 18 ){
     setSelectedHouse('Slytherin')
 }
 },[]);
-
+console.log(count)
     return (
         <div className='quizResults'>
         <h2>Your Hogwarts house is....</h2>
