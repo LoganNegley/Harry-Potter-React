14abba89c6c9b3620ba85d92acbab45b05d1327f
Logan Negley <logannegley@gmail.com>
2020-05-09 14:56:29 -0500

    no changes

--- my-app/src/components/ToggleFavorite.js
+++ my-app/src/components/ToggleFavorite.js
@@ -3,7 +3,30 @@ import React from 'react';
 function ToggleFavorite(){
     return (
         <div className='toggle'>
-            <div className="togggle-button"></div>
+            <div className='button-container'>
+                <p>Slytherin</p>
+                <div className='button'>
+                    <div className= 'inner'></div>
+                </div>
+            </div>
+            <div className='button-container'>
+                <p>Gryffindor</p>
+                <div className='button'>
+                    <div className='inner'></div>
+                </div>
+            </div>
+            <div className='button-container'>
+                <p>Ravenclaw</p>
+                <div className='button'>
+                    <div className='inner'></div>
+                </div>
+            </div>
+            <div className='button-container'>
+                <p>Hufflepuff</p>
+                <div className='button'>
+                    <div className='inner'></div>
+                </div>
+            </div>
         </div>
     );
 };
