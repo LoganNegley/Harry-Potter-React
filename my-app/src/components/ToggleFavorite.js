ae216e43f8565cc0106758b3bf0d7416d32669ff
Logan Negley <logannegley@gmail.com>
2020-05-09 15:26:52 -0500

    toggle buttons created

--- my-app/src/components/ToggleFavorite.js
+++ my-app/src/components/ToggleFavorite.js
@@ -1,30 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 function ToggleFavorite(){
+const [toggle,setToggle] = useState(false);
+
     return (
-        <div className='toggle'>
+        <div className='toggle-container'>
             <div className='button-container'>
                 <p>Slytherin</p>
                 <div className='button'>
-                    <div className= 'inner'></div>
+                    <div className= 'toggle'></div>
                 </div>
             </div>
             <div className='button-container'>
                 <p>Gryffindor</p>
                 <div className='button'>
-                    <div className='inner'></div>
+                    <div className='toggled toggle'></div>
                 </div>
             </div>
             <div className='button-container'>
                 <p>Ravenclaw</p>
                 <div className='button'>
-                    <div className='inner'></div>
+                    <div className='toggle'></div>
                 </div>
             </div>
             <div className='button-container'>
                 <p>Hufflepuff</p>
                 <div className='button'>
-                    <div className='inner'></div>
+                    <div className='toggle'></div>
                 </div>
             </div>
         </div>
